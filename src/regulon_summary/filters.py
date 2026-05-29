be70208a940c8e06713aca3b185da5936d726afc
Luis Angel Muñoz Muñoz <luismm@lcg.unam.mx>
2026-05-28 20:34:23 -0600

    feature: filtrado_minimo_de_genes_pandas

--- src/regulon_summary/filters.py
+++ src/regulon_summary/filters.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 
 def filter_by_min_genes(regulon: pd.DataFrame, min_genes: int) -> pd.DataFrame:
-    return ...
+    return regulon[regulon["genes"].apply(len) >= min_genes]
 
 
 def filter_by_type(regulon: pd.DataFrame, regulator_type: str) -> pd.DataFrame:
     if regulator_type == "all":
-        return ...
+        return regulon
 
-    return ...
+    return regulon[regulon["type"] == regulator_type]
 
 
 def filter_interactions_by_regulon(
@@ -18,4 +18,5 @@ def filter_interactions_by_regulon(
 ) -> pd.DataFrame:
     valid_tfs = set(regulon["TF"])
 
-    return ...
+    # Filtacion de interacciones para conservar aquellas cuyo TF esté presente en el regulón filtrado
+    return interactions[interactions["TF"].isin(valid_tfs)]
