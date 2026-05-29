f5296fc303330b755f74b6b04652b4e9d46bf40c
Luis Angel Muñoz Muñoz <luismm@lcg.unam.mx>
2026-05-28 20:02:01 -0600

    feature: Implementacion para escribir el DataFrame con los campos solicitados

--- src/regulon_summary/exporters.py
+++ src/regulon_summary/exporters.py
@@ -11,14 +11,15 @@ def ensure_output_dir(output_file: str) -> None:
 
 def write_summary(regulon: pd.DataFrame, output_file: str) -> None:
     ensure_output_dir(output_file)
-    ...
+    
+    regulon.to_csv(output_file, sep="\t", index=False, header=True, columns=["TF", "total_genes", "activados", "reprimidos", "genes", "type"])
 
 
 def write_sif(interactions: pd.DataFrame, output_file: str) -> None:
     ensure_output_dir(output_file)
 
     # Crear un DataFrame con TF, effect y gene
-    sif = 
+    sif = interactions[["TF", "effect", "gene"]]
 
     # Guardar el DataFrame en formato SIF (tab-separado, sin encabezado)
-    ...
+    sif.to_csv(output_file, sep="\t", index=False, header=False)
